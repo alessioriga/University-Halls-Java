72a2f77b48bf01e1756eea190f28396e1a24a55b
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-31 18:32:52 +0100

    Update Payment.java

--- src/main/java/ucen/uk/alessiouniversityhall/Payment.java
+++ src/main/java/ucen/uk/alessiouniversityhall/Payment.java
@@ -1,6 +1,7 @@
 package ucen.uk.alessiouniversityhall;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Payment implements Serializable {
@@ -18,5 +19,13 @@ public class Payment implements Serializable {
 
     public Date getPaymentDate() {
         return paymentDate;
+    }
+
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        return "Rent: £" + rent +
+                "\nPayment Date: " +  sdf.format(paymentDate);
     }
 }
