a141a09bff977a7a1b81f0ca0d6eb0334a034c6d
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-31 18:16:03 +0100

    Update Payment.java

--- src/main/java/ucen/uk/alessiouniversityhall/Payment.java
+++ src/main/java/ucen/uk/alessiouniversityhall/Payment.java
@@ -1,4 +1,21 @@
 package ucen.uk.alessiouniversityhall;
 
+import java.util.Date;
+
 public class Payment {
+    private double rent;
+    private Date paymentDate;
+
+    public Payment(double rent) {
+        this.rent = rent;
+        this.paymentDate = new Date();
+    }
+
+    public double getRent() {
+        return rent;
+    }
+
+    public Date getPaymentDate() {
+        return paymentDate;
+    }
 }
