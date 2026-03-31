11d3f5577f8d694f51a9acf9553f7472a29a458f
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-31 18:20:53 +0100

    Update Store.java

--- src/main/java/ucen/uk/alessiouniversityhall/Store.java
+++ src/main/java/ucen/uk/alessiouniversityhall/Store.java
@@ -1,4 +1,22 @@
 package ucen.uk.alessiouniversityhall;
 
+import java.util.ArrayList;
+
 public class Store {
+    private ArrayList<Person> people;
+    private int index;
+
+    public Store() {
+        people = new ArrayList<>();
+        index = 0;
+    }
+
+    public ArrayList<Person> getPeople() {
+        return people;
+    }
+
+    public void setPeople(ArrayList<Person> people) {
+        this.people = people;
+        index = 0;
+    }
 }
