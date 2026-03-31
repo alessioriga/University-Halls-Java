dde5097f5ce8776d18fe759502f1b85ffb370638
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-31 17:05:00 +0100

    Update Person.java

--- src/main/java/ucen/uk/alessiouniversityhall/Person.java
+++ src/main/java/ucen/uk/alessiouniversityhall/Person.java
@@ -1,4 +1,37 @@
 package ucen.uk.alessiouniversityhall;
 
 public class Person {
+    private String fullName;
+    private String dob;
+    private String nationality;
+    private String gender;
+    private String hall;
+
+    public Person(String fullName, String dob, String nationality, String gender, String hall) {
+        this.fullName = fullName;
+        this.dob = dob;
+        this.nationality = nationality;
+        this.gender = gender;
+        this.hall = hall;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getHall() {
+        return hall;
+    }
 }
