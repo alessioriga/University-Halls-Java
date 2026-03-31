25650cae9a5a6d745b0e0d9d35a54110b700018f
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-31 17:32:12 +0100

    Update Hall.java

--- src/main/java/ucen/uk/alessiouniversityhall/Hall.java
+++ src/main/java/ucen/uk/alessiouniversityhall/Hall.java
@@ -1,4 +1,37 @@
 package ucen.uk.alessiouniversityhall;
 
+import java.util.ArrayList;
+
 public class Hall {
+    private String hallName;
+    private String dietType;
+    private boolean groundFloorAvailable;
+    private ArrayList<Person> residents;
+
+    public Hall(String hallName, String dietType, boolean groundFloorAvailable) {
+        this.hallName = hallName;
+        this.dietType = dietType;
+        this.groundFloorAvailable = groundFloorAvailable;
+        this.residents = new ArrayList<>();
+    }
+
+    public String getHallName() {
+        return hallName;
+    }
+
+    public String getDietType() {
+        return dietType;
+    }
+
+    public boolean isGroundFloorAvailable() {
+        return groundFloorAvailable;
+    }
+
+    public ArrayList<Person> getResidents() {
+        return residents;
+    }
+
+    public void addResident(Person p) {
+        this.residents.add(p);
+    }
 }
