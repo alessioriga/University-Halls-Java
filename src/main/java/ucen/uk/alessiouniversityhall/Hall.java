22870a01c8031af493e1ca7d01e47903a157ab68
Alessio Rigamonti <alessioriga93@hotmail.it>
2026-03-31 18:30:01 +0100

    Update Hall.java

--- src/main/java/ucen/uk/alessiouniversityhall/Hall.java
+++ src/main/java/ucen/uk/alessiouniversityhall/Hall.java
@@ -1,8 +1,9 @@
 package ucen.uk.alessiouniversityhall;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Hall {
+public class Hall implements Serializable {
     private String hallName;
     private String dietType;
     private boolean groundFloorAvailable;
