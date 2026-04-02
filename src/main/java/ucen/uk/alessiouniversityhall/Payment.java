9f043f7e4fda63e63b4c7e0dde2c91abdb92d583
Alessio Rigamonti <159440780+alessioriga@users.noreply.github.com>
2026-04-02 16:47:56 +0100

    Update Payment.java

--- src/main/java/ucen/uk/alessiouniversityhall/Payment.java
+++ src/main/java/ucen/uk/alessiouniversityhall/Payment.java
@@ -4,25 +4,56 @@ import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+/**
+ * This class represents a payment for student accommodation.
+ * It stores the rent amount and the date when the payment was created.
+ * This class implements Serializable so payment data can be saved
+ * and loaded from a text file.
+ *
+ * @author Alessio
+ */
 public class Payment implements Serializable {
+
     private final double rent;
     private final Date paymentDate;
 
+    /**
+     * Constructs a new Payment with rent amount and current date.
+     *
+     * @param rent amount in GBP
+     */
     public Payment(double rent) {
         this.rent = rent;
         this.paymentDate = new Date();
     }
 
+    /**
+     * Returns the rent amount.
+     *
+     * @return rent
+     */
     public double getRent() {
         return rent;
     }
 
+    /**
+     * Returns the payment date.
+     *
+     * @return payment date
+     */
     public Date getPaymentDate() {
         return paymentDate;
     }
 
+    /**
+     * Returns a formatted string representation of the payment.
+     * Includes rent and payment date.
+     *
+     * @return formatted payment information
+     */
     @Override
     public String toString() {
+
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
         return "Rent: £" + rent +
