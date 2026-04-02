5d4e9e4da61951bdeb02133b11d816aecaeee158
Alessio Rigamonti <159440780+alessioriga@users.noreply.github.com>
2026-04-02 13:16:57 +0100

    Update AlessioUniversityHallApp.java

--- src/main/java/ucen/uk/alessiouniversityhall/AlessioUniversityHallApp.java
+++ src/main/java/ucen/uk/alessiouniversityhall/AlessioUniversityHallApp.java
@@ -1,11 +1,18 @@
 package ucen.uk.alessiouniversityhall;
 
 /**
+ * This class starts the university hall application.
+ * It launches the Graphical User Interface.
  *
  * @author Alessio
  */
 public class AlessioUniversityHallApp {
 
+    /**
+     * Starts the application and opens the Graphical User Interface.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         new AlessioUniGUI();
     }
